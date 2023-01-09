b7190b7fe2ce109013a33c542a3d20232e239bfd
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2023-01-09 16:02:13 -0600

    add teams and presidents api

--- api/index.js
+++ api/index.js
@@ -1,18 +1,44 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/serve-static.module';
-import leaderboard from '../db/leaderboard';
+import leaderboard from '../db/leaderboard.json';
+import teams from '../db/teams.json';
+import presidents from '../db/presidents.json';
 
 const app = new Hono();
 
 app.get('/', (ctx) => {
-  return ctx.json({
-    endpoint: '/leaderboard',
-    description: 'Returns the leaderboard'
-  });
+  return ctx.json(
+    {
+      endpoint: '/leaderboard',
+      description: 'Returns the leaderboard'
+    },
+    {
+      endpoint: '/teams',
+      description: 'Returns the teams'
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns the presidents'
+    }
+  );
 });
 
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard);
+});
+
+app.get('/teams', (ctx) => {
+  return ctx.json(teams);
+});
+
+app.get('/presidents', (ctx) => {
+  return ctx.json(presidents);
+});
+
+app.get('/presidents/:id', (ctx) => {
+  const id = ctx.req.param('id');
+  const foundPresident = presidents.find((president) => president.id === id);
+  return foundPresident ? ctx.json(foundPresident) : ctx.json({ message: 'President not found' }, 404);
 });
 
 app.get('/static/*', serveStatic({ root: './' }));
