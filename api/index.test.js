bb8b6c7eb4e3ec563d5068ac23513d73cee348ca
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2023-01-09 16:59:09 -0600

    fix linter test

--- api/index.test.js
+++ api/index.test.js
@@ -1,15 +1,11 @@
-import { unstable_dev } from 'wrangler';
+import { unstable_dev as UnStableDev } from 'wrangler';
 import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 
 describe('Worker', () => {
   let worker;
 
   beforeAll(async () => {
-    worker = await unstable_dev(
-      'src/index.js',
-      {},
-      { disableExperimentalWarning: true }
-    );
+    worker = await UnStableDev('src/index.js', {}, { disableExperimentalWarning: true });
   });
 
   afterAll(async () => {
@@ -20,7 +16,7 @@ describe('Worker', () => {
     const resp = await worker.fetch();
     if (resp) {
       const text = await resp.text();
-      expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+      expect(text).toMatchInlineSnapshot('Hello World!');
     }
   });
 });
