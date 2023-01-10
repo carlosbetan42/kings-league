bcd59c1f6024dcbdc50f4cc9a24dadce16e8288a
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2023-01-10 16:22:19 -0600

    Fix optional trailing slash

--- api/index.js
+++ api/index.js
@@ -23,27 +23,25 @@ app.get('/', (ctx) => {
   ]);
 });
 
-app.get('/leaderboard', (ctx) => {
+app.get('/leaderboard\\/?', (ctx) => {
   return ctx.json(leaderboard);
 });
 
-app.get('/teams', (ctx) => {
+app.get('/teams\\/?', (ctx) => {
   return ctx.json(teams);
 });
 
-app.get('/teams/:id', (ctx) => {
+app.get('/teams/:id\\/?', (ctx) => {
   const id = ctx.req.param('id');
   const foundTeam = teams.find((team) => team.id === id);
-  return foundTeam
-    ? ctx.json(foundTeam)
-    : ctx.json({ message: 'Team not found' }, 404);
+  return foundTeam ? ctx.json(foundTeam) : ctx.json({ message: 'Team not found' }, 404);
 });
 
-app.get('/presidents', (ctx) => {
+app.get('/presidents\\/?', (ctx) => {
   return ctx.json(presidents);
 });
 
-app.get('/presidents/:id', (ctx) => {
+app.get('/presidents/:id\\/?', (ctx) => {
   const id = ctx.req.param('id');
   const foundPresident = presidents.find((president) => president.id === id);
   return foundPresident
