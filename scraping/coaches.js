2e970f8e100ccdc64fda853344668c53db8053e8
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2023-01-11 12:53:16 -0600

    Add coaches scraper

---
+++ scraping/coaches.js
@@ -0,0 +1,33 @@
+const INFO_COACHES_SELECTORS = {
+  teamName: { selector: '.name.mt10', typeOf: 'string' },
+  coach: { selector: '.name.mt20', typeOf: 'string' },
+  coachImg: { selector: '.player-circle-box', typeOf: 'string' }
+};
+
+export const getCoaches = async ($) => {
+  const coachesTeam = $(INFO_COACHES_SELECTORS.coach.selector)
+    .toArray()
+    .map((coachName) => coachName.children[0].data);
+
+  const coachsImgTeam = $(INFO_COACHES_SELECTORS.coachImg.selector)
+    .toArray()
+    .map((coachImg) => {
+      const { attribs } = coachImg;
+      const { src } = attribs;
+      return src;
+    });
+
+  const teamsName = $(INFO_COACHES_SELECTORS.teamName.selector)
+    .toArray()
+    .map((teamName) => teamName.children[0].data);
+
+  const teamsWithCoach = coachesTeam.map((coach, i) => {
+    return {
+      name: coach,
+      teamName: teamsName[i],
+      image: coachsImgTeam[i]
+    };
+  });
+
+  return teamsWithCoach;
+};
