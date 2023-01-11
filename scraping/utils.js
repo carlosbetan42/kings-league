dd937c83d7b92ae334abdf335be081d065c7a0e1
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2023-01-11 11:31:11 -0600

    Add utils functions to global

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,8 +1,18 @@
 import * as cheerio from 'cheerio';
+import { writeDBFile } from '../db/index.js';
+import { getLeaderboard } from './leaderboard.js';
+import { logError, logInfo, logSuccess } from './log.js';
+import { getMvp } from './mvp.js';
 
-export const URLS = {
-  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
-  mvp: 'https://kingsleague.pro/estadisticas/mvp/'
+export const SCRAPINGS = {
+  leaderboard: {
+    url: 'https://kingsleague.pro/estadisticas/clasificacion/',
+    scraper: getLeaderboard
+  },
+  mvp: {
+    url: 'https://kingsleague.pro/estadisticas/mvp/',
+    scraper: getMvp
+  }
 };
 
 export const cleanText = (text) =>
@@ -11,8 +21,30 @@ export const cleanText = (text) =>
     .replace(/.*:/g, '')
     .trim();
 
-export async function scrape(url) {
+export const scrape = async (url) => {
   const res = await fetch(url);
   const html = await res.text();
   return cheerio.load(html);
-}
+};
+
+export const scrapeAndSave = async (name) => {
+  const start = performance.now();
+  try {
+    const { scraper, url } = SCRAPINGS[name];
+
+    logInfo(`Scraping ${name}...`);
+    const $ = await scrape(url);
+    const content = await scraper($);
+    logSuccess(`${name} list scraped successfully`);
+    logInfo(`Writing ${name} list to DB...`);
+    await writeDBFile(name, content);
+    logSuccess(`${name} list scraped successfully`);
+  } catch (e) {
+    logError(`Error scraping ${name} list`);
+    logError(e);
+  } finally {
+    const end = performance.now();
+    const time = (end - start) / 1000;
+    logInfo(`${name} list scraped in ${time} seconds`);
+  }
+};
